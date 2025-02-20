5e4f22d04ac265f44389333a9ba16c3d96b52515
Kenneth Brewer <101129262+kenbrew96@users.noreply.github.com>
2025-02-20 07:06:12 -0500

    Update lossless_compression.py

--- Data-Compression/lossless_compression.py
+++ Data-Compression/lossless_compression.py
@@ -1,1 +1,10 @@
+import gzip
+
+def compress_data(input_file, output_file):
+    with open(input_file, 'rb') as f_in:
+        with gzip.open(output_file, 'wb') as f_out:
+            f_out.writelines(f_in)
+
+if __name__ == '__main__':
+    compress_data('data/input.txt', 'data/output.gz')
 
