fa8beaec43f053f3454fb8399fca7cad9baa589a
Kenneth Brewer <101129262+kenbrew96@users.noreply.github.com>
2025-02-20 07:10:51 -0500

    Update data_pipeline.py

--- Flow-Composition/data_pipeline.py
+++ Flow-Composition/data_pipeline.py
@@ -1,1 +1,16 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime
+
+def build_data_pipeline():
+    dag = DAG('data_pipeline', description='Simple Data Pipeline',
+              schedule_interval='@daily', start_date=datetime(2022, 1, 1), catchup=False)
+
+    start_task = DummyOperator(task_id='start', dag=dag)
+    end_task = DummyOperator(task_id='end', dag=dag)
+
+    start_task >> end_task
+
+if __name__ == '__main__':
+    build_data_pipeline()
 
