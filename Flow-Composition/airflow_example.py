f7e004db9eb929a775c4bd3eea6ab67a9ef3af7d
Kenneth Brewer <101129262+kenbrew96@users.noreply.github.com>
2025-02-20 07:11:23 -0500

    Update airflow_example.py

--- Flow-Composition/airflow_example.py
+++ Flow-Composition/airflow_example.py
@@ -1,1 +1,17 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
+def hello_world():
+    print("Hello, world!")
+
+def build_airflow_example():
+    dag = DAG('airflow_example', description='Simple Airflow Example',
+              schedule_interval='@daily', start_date=datetime(2022, 1, 1), catchup=False)
+
+    hello_task = PythonOperator(task_id='hello', python_callable=hello_world, dag=dag)
+    hello_task
+
+if __name__ == '__main__':
+    build_airflow_example()
 
