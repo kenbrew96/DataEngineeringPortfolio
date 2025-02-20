f069e581bd5f06d4c6dbfce89a525ced54f1b7d6
Kenneth Brewer <101129262+kenbrew96@users.noreply.github.com>
2025-02-20 07:12:55 -0500

    Update network_firewall_setup.py

--- Firewalls/network_firewall_setup.py
+++ Firewalls/network_firewall_setup.py
@@ -1,1 +1,9 @@
+import os
+
+def setup_network_firewall():
+    os.system("ufw allow from 192.168.1.0/24 to any port 80")
+    os.system("ufw enable")
+
+if __name__ == '__main__':
+    setup_network_firewall()
 
