423c978a1def91be72693c51e98ea4ce78d613af
Kenneth Brewer <101129262+kenbrew96@users.noreply.github.com>
2025-02-20 06:54:44 -0500

    Update file_storage_setup.py

--- Cloud-Storage/file_storage_setup.py
+++ Cloud-Storage/file_storage_setup.py
@@ -1,1 +1,9 @@
+import boto3
+
+def upload_file_to_s3(file_name, bucket_name):
+    s3 = boto3.client('s3')
+    s3.upload_file(file_name, bucket_name, file_name)
+
+if __name__ == '__main__':
+    upload_file_to_s3('data/sample.csv', 'my-new-bucket')
 
