ca161554ae0d60715ba6b4bae6af283a824d636a
Kenneth Brewer <101129262+kenbrew96@users.noreply.github.com>
2025-02-20 07:16:55 -0500

    Update ip_whitelisting.py

--- Whitelisting/ip_whitelisting.py
+++ Whitelisting/ip_whitelisting.py
@@ -1,1 +1,8 @@
+import subprocess
+
+def setup_ip_whitelist():
+    subprocess.run(["ufw", "allow", "from", "192.168.1.100", "to", "any", "port", "80"])
+
+if __name__ == '__main__':
+    setup_ip_whitelist()
 
