9d5e71a803f44c6a90b7d8740231f91ef4ea5785
Kenneth Brewer <101129262+kenbrew96@users.noreply.github.com>
2025-02-20 07:04:24 -0500

    Update lossy_compression.py

--- Data-Compression/lossy_compression.py
+++ Data-Compression/lossy_compression.py
@@ -1,1 +1,10 @@
+from PIL import Image
+
+def compress_image(input_image, output_image):
+    img = Image.open(input_image)
+    img.save(output_image, "JPEG", quality=50)
+
+if __name__ == '__main__':
+    compress_image('images/input.jpg', 'images/output.jpg')
+
 
