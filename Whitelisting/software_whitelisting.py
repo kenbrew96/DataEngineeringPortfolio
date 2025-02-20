ea0948582a3ed409f1f02e02c348899470b27677
Kenneth Brewer <101129262+kenbrew96@users.noreply.github.com>
2025-02-20 07:17:05 -0500

    Update software_whitelisting.py

--- Whitelisting/software_whitelisting.py
+++ Whitelisting/software_whitelisting.py
@@ -1,1 +1,8 @@
+import subprocess
+
+def setup_software_whitelist():
+    subprocess.run(["ufw", "allow", "from", "192.168.1.100", "to", "any", "port", "22"])
+
+if __name__ == '__main__':
+    setup_software_whitelist()
 
