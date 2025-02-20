b7d6478993b9be5e1f366d276b5e096ab60d0bbe
Kenneth Brewer <101129262+kenbrew96@users.noreply.github.com>
2025-02-20 07:13:15 -0500

    Update host_firewall_setup.py

--- Firewalls/host_firewall_setup.py
+++ Firewalls/host_firewall_setup.py
@@ -1,1 +1,9 @@
+import os
+
+def setup_host_firewall():
+    os.system("ufw allow from 192.168.1.0/24 to any port 22")
+    os.system("ufw enable")
+
+if __name__ == '__main__':
+    setup_host_firewall()
 
