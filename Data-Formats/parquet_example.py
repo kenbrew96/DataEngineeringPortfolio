b7a3f87e85680182df722469c167e6bc74f9bc00
Kenneth Brewer <101129262+kenbrew96@users.noreply.github.com>
2025-02-20 07:00:49 -0500

    Update parquet_example.py

--- Data-Formats/parquet_example.py
+++ Data-Formats/parquet_example.py
@@ -1,1 +1,10 @@
+import pandas as pd
+
+def read_parquet():
+    data = pd.read_parquet('data/input.parquet')
+    return data
+
+if __name__ == '__main__':
+    data = read_parquet()
+    print(data)
 
