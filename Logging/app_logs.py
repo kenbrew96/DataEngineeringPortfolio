8e7defe31580e91afba2097ea78ffff24a26cecd
Kenneth Brewer <101129262+kenbrew96@users.noreply.github.com>
2025-02-20 07:22:03 -0500

    Update app_logs.py

--- Logging/app_logs.py
+++ Logging/app_logs.py
@@ -1,1 +1,9 @@
+import logging
+
+def setup_application_logging():
+    logging.basicConfig(level=logging.DEBUG)
+    logging.debug('Application logging setup completed.')
+
+if __name__ == '__main__':
+    setup_application_logging()
 
