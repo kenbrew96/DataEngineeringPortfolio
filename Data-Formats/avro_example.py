925e69ac5864d03def2788a2a316c344ed29cd47
Kenneth Brewer <101129262+kenbrew96@users.noreply.github.com>
2025-02-20 07:01:03 -0500

    Update avro_example.py

--- Data-Formats/avro_example.py
+++ Data-Formats/avro_example.py
@@ -1,1 +1,12 @@
+import fastavro
+from fastavro import reader
+
+def read_avro():
+    with open('data/input.avro', 'rb') as f:
+        avro_reader = reader(f)
+        for record in avro_reader:
+            print(record)
+
+if __name__ == '__main__':
+    read_avro()
 
