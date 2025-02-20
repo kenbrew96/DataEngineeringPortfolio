0b56b531dce77fb57753a028ff4229c66f8bb9a6
Kenneth Brewer <101129262+kenbrew96@users.noreply.github.com>
2025-02-20 07:00:26 -0500

    Update json_example.py

--- Data-Formats/json_example.py
+++ Data-Formats/json_example.py
@@ -1,1 +1,11 @@
+import json
+
+def read_json():
+    with open('data/input.json', 'r') as file:
+        data = json.load(file)
+    return data
+
+if __name__ == '__main__':
+    data = read_json()
+    print(data)
 
