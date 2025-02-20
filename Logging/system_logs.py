0f9504e80ca26f6d1897852d0bf3e67624fc0460
Kenneth Brewer <101129262+kenbrew96@users.noreply.github.com>
2025-02-20 07:22:27 -0500

    Update system_logs.py

--- Logging/system_logs.py
+++ Logging/system_logs.py
@@ -1,1 +1,9 @@
+import logging
+
+def setup_system_logging():
+    logging.basicConfig(level=logging.INFO)
+    logging.info('System logging setup completed.')
+
+if __name__ == '__main__':
+    setup_system_logging()
 
