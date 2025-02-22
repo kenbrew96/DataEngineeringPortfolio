2bbe044f31e8734c7fd30e2b50ab67565d6dec19
Kenneth Brewer <101129262+kenbrew96@users.noreply.github.com>
2025-02-21 19:31:17 -0500

    Update risk_assessment.py

--- use_cases/ATOs-DMV-Digital-Transformation/risk_assessment.py
+++ use_cases/ATOs-DMV-Digital-Transformation/risk_assessment.py
@@ -1,1 +1,9 @@
+import os
 
+def check_compliance():
+    os.system("nmap -A -T4 dmv.maryland.gov > security_scan.txt")
+    os.system("aws iam list-roles > iam_roles.txt")
+    print("✅ Security compliance checks completed.")
+
+if __name__ == "__main__":
+    check_compliance()
